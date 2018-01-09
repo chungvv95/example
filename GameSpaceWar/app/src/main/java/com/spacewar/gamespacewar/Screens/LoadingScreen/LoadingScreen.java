71ec93af26293b32151d10e6a11b4b5f2a5e9bb7
tienvv91 <vuvantien91@gmail.com>
2018-01-10 00:07:39 +0700

    Add loading screen.

--- GameSpaceWar/app/src/main/java/com/spacewar/gamespacewar/Screens/LoadingScreen/LoadingScreen.java
+++ GameSpaceWar/app/src/main/java/com/spacewar/gamespacewar/Screens/LoadingScreen/LoadingScreen.java
@@ -17,60 +17,89 @@ import com.spacewar.gamespacewar.Stores.GameStatic;
  */
 
 public class LoadingScreen extends ScreenBase {
+    private Bitmap bmLoadingBg;
+    private Bitmap bmProgressBg;
+    private Bitmap bmProgressPoint;
+
+    private static final String loadingFolder = "loadingScreen";
+
     Paint paint = new Paint();
     private int X = 0;
     private int Y = 0;
     private int R = 200;
     Rect rect = new Rect();
+    Rect recProgressBarBG = new Rect();
+    private int proLenght = 0;
     private  boolean initMenuScreen = false;
     private  boolean initOptionScreen = false;
     private  boolean initPlayGameScreen = false;
     private  boolean initHighScoreScreen = false;
-    private  final float timeMin = 2.0f;
+    private  final float timeMin = 100.0f;
     private  float timeTick = 0;
+    private  float progressing = 0;
+
+
     public LoadingScreen(GameActivityInterface game, SCREEN_TYPE type) {
         super(game, type);
-        GameBitmap.getInstance().addImgFormFolder("loadingScreen");
+        GameBitmap gb = GameBitmap.getInstance();
+        gb.addImgFormFolder(loadingFolder);
+        bmLoadingBg = gb.getBitmap(loadingFolder + "/loading_bg.png");
+        bmProgressBg = gb.getBitmap(loadingFolder + "/loading_ps.png");
+        bmProgressPoint = gb.getBitmap(loadingFolder + "/loading_point.png");
     }
 
     @Override
     public void Draw(Canvas canvas) {
         GameBitmap gb = GameBitmap.getInstance();
         paint.setColor(Color.BLUE);
+        rect.set(0,0,this.getWidth(), this.getHeigh());
+        canvas.drawBitmap(bmLoadingBg, null, rect,null);
 
-        Bitmap bm = gb.getBitmap("loadingScreen/menu_bg.png");
-//            canvas.drawBitmap(bm, null, new Rect(0,0,GameStatic.ScreenSize.x, GameStatic.ScreenSize.y),null);
-        canvas.drawRect(new Rect(this.X,this.Y,X + 10,Y +10), paint);
+        recProgressBarBG.set(getWidth() / 4 , getHeigh() * 4 / 5, getWidth() / 4  + proLenght, getHeigh() * 4 / 5 + bmProgressBg.getHeight());
+        canvas.drawBitmap(bmProgressBg, null, recProgressBarBG, null);
+
+        canvas.drawBitmap(bmProgressPoint, recProgressBarBG.right - (bmProgressPoint.getWidth() /4), recProgressBarBG.top, null);
     }
 
     @Override
     public void Update(float deltaTime) {
         Resources rs = GameStatic.context.getResources();
         timeTick += deltaTime;
-        X = (int) (this.R * Math.sin(timeTick)) + 300;
-        Y = (int) (this.R * Math.cos(timeTick)) + 100;
+        int max = getWidth() / 2;
         if(initMenuScreen == false) {
             initMenuScreen = true;
             // init menu game
             addMenuFolder();
+            proLenght += max / 8;
 
         }else if (initHighScoreScreen == false) {
             initHighScoreScreen = true;
+            proLenght += max / 8;
 
         } else if(initOptionScreen == false) {
             initOptionScreen = true;
+            proLenght += max / 8;
 
         } else if(initPlayGameScreen == false) {
             initPlayGameScreen = true;
+            proLenght += max / 8;
         }
 
         if(initPlayGameScreen &&
                 initOptionScreen &&
                 initHighScoreScreen &&
-                initMenuScreen &&
-                timeTick > timeMin)
+                initMenuScreen )
         {
-            game.goToScreen(SCREEN_TYPE.SCEEN_MENU);
+            progressing += deltaTime;
+            if(progressing > 0.3){
+                progressing = 0;
+                proLenght += 10;
+                if(proLenght >= getWidth() / 2 )
+                    proLenght = getWidth() / 2;
+            }
+        }
+        if(proLenght == getWidth() / 2) {
+            this.game.goToScreen(SCREEN_TYPE.SCEEN_MENU);
         }
     }
     private void addMenuFolder(){
